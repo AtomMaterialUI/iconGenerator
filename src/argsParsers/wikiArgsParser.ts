4065106ab7e9232e3bdd88f2cd2fa3fd326a4885
helio <heliosaian@gmail.com>
2020-04-01 11:50:57 +0300

    push the icons

--- src/argsParsers/wikiArgsParser.ts
+++ src/argsParsers/wikiArgsParser.ts
@@ -1,26 +1,28 @@
 /*
- * The MIT License (MIT)
  *
- * Copyright (c) 2020 Chris Magnussen and Elior Boukhobza
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
+ *  * The MIT License (MIT)
+ *  *
+ *  * Copyright (c) 2020 Elior "Mallowigi" Boukhobza
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *  *
+ *  *
  *
  */
 
@@ -109,13 +111,13 @@ export class WikiArgsParser {
       command: pargs._[0] as ExamplesFlags,
       account: pargs.account as string,
       output: pargs.out as WikiAllowedOutputs,
-      token: pargs.token as string,
+      token: (pargs.token || process.env.token) as string,
       force: pargs.force as boolean,
     };
   }
 
   private validate(argv: yargs.Arguments<yargs.Omit<{}, keyof any> & InferredArgs>) {
-    if (argv.out === WikiAllowedOutputs.REPO && !argv.token) {
+    if (argv.out === WikiAllowedOutputs.REPO && !argv.token && !process.env.token) {
       yargs.showHelp();
       this.logger.error(`No token provided`);
       process.exit(1);
